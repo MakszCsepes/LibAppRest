0012561dad08944a5a07d00f20652c00eb150b93
MakszMasodik <max.cheypesh@gmail.com>
2025-03-19 15:04:13 +0000

    Adding GitHub Actions workflow files

---
+++ functions_to_test.py
@@ -0,0 +1,19 @@
+
+def return_42():
+    return 42
+
+def mult_method(a, b):
+    return a * b
+
+
+def test_F():
+    assert False
+
+def test_42():
+    assert return_42() == 42
+
+def test_mult_method_1():
+    assert mult_method(2, 5) == 10
+
+def test_mult_method_2():
+    assert mult_method(4, 7) == 22
